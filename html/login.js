b170a209834622af4a4bd6582888a5e3565249ff
ksdyb.park <ksdyb@amazon.com>
2023-10-23 20:35:50 +0900

    update

--- html/login.js
+++ html/login.js
@@ -1,5 +1,6 @@
 const myForm = document.querySelector('#my-form');
 const userInput = document.querySelector('#userId');
+const convtypeInput = document.querySelector('#convtype');
 
 myForm.addEventListener('submit', onSubmit);
 
@@ -9,14 +10,27 @@ if(userId != '') {
     userInput.value = userId;
 }
 
+let conversationType = localStorage.getItem('convType'); // set conversationType if exists 
+if(conversationType != '') {
+    convtypeInput.value = conversationType;
+}
+else {
+    convtypeInput.value = "normal"  // general conversation
+}
+
 console.log(userInput.value);
+console.log(convtypeInput.value);
 
 function onSubmit(e) {
     e.preventDefault();
     console.log(userInput.value);
+    console.log(convtypeInput.value);
 
     localStorage.setItem('userId',userInput.value);
     console.log('Save Profile> userId:', userInput.value)    
+
+    localStorage.setItem('convType',convtypeInput.value);
+    console.log('Save Profile> convtype:', convtypeInput.value)
 
     window.location.href = "chat.html";
 }
