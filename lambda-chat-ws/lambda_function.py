e05f4cd8e9ef2eedf8e33561cc07670a808b238f
ksdyb.park <ksdyb@amazon.com>
2023-12-05 23:45:06 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -342,15 +342,6 @@ def getAllowTime():
 
     return timeStr
 
-def isTyping(connectionId, requestId):    
-    msg_proceeding = {
-        'request_id': requestId,
-        'msg': 'Proceeding...',
-        'status': 'istyping'
-    }
-    #print('result: ', json.dumps(result))
-    sendMessage(connectionId, msg_proceeding)
-
 def readStreamMsg(connectionId, requestId, stream):
     msg = ""
     if stream:
@@ -442,9 +433,9 @@ def getResponse(connectionId, jsonBody):
                 if conversationMode == 'true':
                     if convType == 'qa' or convType == 'normal':
                         conversation.prompt = get_prompt_template(text, convType)
-                        isTyping(connectionId, requestId) 
                         stream = conversation.predict(input=text)
-                        #print('stream: ', stream)                            
+                        #print('stream: ', stream)
+                            
                         msg = readStreamMsg(connectionId, requestId, stream)
                             
                         # extract chat history for debug
@@ -456,7 +447,6 @@ def getResponse(connectionId, jsonBody):
                     else: # no need history, translation, sentiment, extraction
                         PROMPT = get_prompt_template(text, convType)
                         #print('PROMPT: ', PROMPT)
-                        isTyping(connectionId, requestId) 
                         stream = llm(PROMPT.format(input=text))
                         msg = readStreamMsg(connectionId, requestId, stream)
 
