c6331ecbd2a82d9243059159a3af6a00bea5d186
ksdyb.park <ksdyb@amazon.com>
2024-01-05 11:10:55 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -18,21 +18,30 @@ if(protocol == 'WEBSOCKET') {
     webSocket = connect(endpoint, 'initial');
 }
 
+let undelivered = new HashMap();
+let retry_count = 0;
 function sendMessage(message) {
     if(!isConnected) {
         console.log('reconnect...'); 
         webSocket = connect(endpoint, 'reconnect');
         
         if(langstate=='korean') {
-            addNotifyMessage("연결중입니다. 잠시후 다시시도하세요.");
+            addNotifyMessage("재연결중입니다. 연결후 자동 재전송합니다.");
         }
         else {
-            addNotifyMessage("We are connecting again. Please wait a few seconds.");                        
+            addNotifyMessage("We are connecting again. Your message will be retried after connection.");                        
         }
+
+        undelivered.put(message.request_id, message);
+        console.log('undelivered message: ', message);
+        
+        return false
     }
     else {
         webSocket.send(JSON.stringify(message));     
         console.log('message: ', message);   
+
+        return true;
     }     
 }
 
@@ -58,8 +67,28 @@ function connect(endpoint, type) {
         console.log('connected...');
         isConnected = true;
 
+        if(undelivered.size() && retry_count>0) {
+            let keys = undelivered.getKeys();
+            console.log('retry undelived messags!');            
+            console.log('keys: ', keys);
+            console.log('retry_count: ', retry_count);
+
+            for(i in keys) {
+                let message = undelivered.get(keys[i])
+                console.log('message', message)
+                if(!sendMessage(message)) break;
+                else {
+                    undelivered.remove(message.request_id)
+                }
+            }
+            retry_count--;
+        }
+        else {
+            retry_count = 3
+        }
+
         if(type == 'initial')
-            setInterval(ping, 57000);  // ping interval: 57 seconds
+            setInterval(ping, 40000);  // ping interval: 40 seconds
     };
 
     // message 
@@ -143,14 +172,12 @@ HashMap.prototype = {
     get: function(key) {
         return this.map[key];
     },
-    getAll: function() {
-        return this.map;
-    },
-    clear: function() {
-        return this.map;
-    },
-    isEmpty: function() {
-        return (this.map.size()==0);
+    size: function() {
+        var keys = new Array();
+        for(i in this.map) {
+            keys.push(i);
+        }
+        return keys.length;
     },
     remove: function(key) {
         delete this.map[key];
