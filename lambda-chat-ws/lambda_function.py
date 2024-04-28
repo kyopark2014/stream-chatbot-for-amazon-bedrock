fe170b5cce632137addd642cc3c3a3b2ba75750e
ksdyb.park <ksdyb@amazon.com>
2024-04-28 11:36:33 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -239,9 +239,6 @@ def isKorean(text):
         return False
 
 def general_conversation(connectionId, requestId, chat, query):
-    global time_for_inference, history_length, token_counter_history    
-    time_for_inference = history_length = token_counter_history = 0
-    
     if isKorean(query)==True :
         system = (
             "다음의 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연이고, 모르는 질문을 받으면 솔직히 모른다고 말합니다."
@@ -269,12 +266,11 @@ def general_conversation(connectionId, requestId, chat, query):
             }
         )
         msg = readStreamMsg(connectionId, requestId, stream.content)    
-        print('strema output: ', stream)
         
         usage = stream.response_metadata['usage']
         print('prompt_tokens: ', usage['prompt_tokens'])
         print('completion_tokens: ', usage['completion_tokens'])
-        print('total_tokens: ', usage['total_tokens'])        
+        print('total_tokens: ', usage['total_tokens'])
         msg = stream.content
 
     except Exception:
